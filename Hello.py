7afbc190c96b18dfe616a4dd997d964c0f530bb0
MOLLA SAROYAR HOSSAIN <55195851+molla1998@users.noreply.github.com>
2023-11-14 19:53:55 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -10,5 +10,9 @@ st.write(amount_p)
 spent = (amount/350000)*100
 #print(spent)
 result = st.slider('Amount Spent Percentage', 0, 100, round(spent))
-Details = '[Spending Details](https://docs.google.com/spreadsheets/d/1uYHIpqs-cm0l-PMWME5GUpxgSNCa3NNt/edit?usp=drive_link&ouid=102403513019249144512&rtpof=true&sd=true)'
-st.markdown(Details, unsafe_allow_html=True)
+
+Details1 = '[Spending Details (Current)](https://docs.google.com/spreadsheets/d/12Yan1dsOCCYXrkRDGmZ4CXrQY-sZP-vB/edit?usp=sharing&ouid=102403513019249144512&rtpof=true&sd=true)'
+st.markdown(Details1, unsafe_allow_html=True)
+
+Details2 = '[Spending Details (Old)](https://docs.google.com/spreadsheets/d/1uYHIpqs-cm0l-PMWME5GUpxgSNCa3NNt/edit?usp=drive_link&ouid=102403513019249144512&rtpof=true&sd=true)'
+st.markdown(Details2, unsafe_allow_html=True)
