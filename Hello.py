5c70158a8ef6ff69d56286badcd6e150830b7de8
MOLLA SAROYAR HOSSAIN <55195851+molla1998@users.noreply.github.com>
2023-12-23 23:43:15 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from datetime import date
 
-amount=201866 #Update Here Daily Spending 19/11/2023
+amount=207866 #Update Here Daily Spending 23/12/2023
 
 st.header('Project E', divider='rainbow')
 today = date.today()
